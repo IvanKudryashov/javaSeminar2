4919eab22936d5692ec943b83deb8a1ba4f9b61e
legolav <bbabkova4@gmail.com>
2023-06-10 10:43:07 +0300

    добавлено дз

---
+++ task4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/* Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. 
+Пользователю должно показаться сообщение, что пустые строки вводить нельзя. */
+
+public class task4 {
+    public static void main(String[] args) {
+        System.out.println("введите строку");
+        Scanner scanner = new Scanner(System.in);
+
+        EmptyStringException(scanner.nextLine());
+    }
+
+    public static void EmptyStringException(String stringValue) {
+        if (stringValue.isEmpty()) {
+            throw new RuntimeException("Пустые строки вводить нельзя!");
+        }
+    }
+}
